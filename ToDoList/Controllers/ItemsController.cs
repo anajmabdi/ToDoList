08df26171d37ed84ba67760ae6cdbe22185def10
Najma Admin <najmabdi@Najmas-MacBook-Air.local>
2023-02-14 20:24:46 -0800

    it will search and get items in list

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -7,24 +7,30 @@ namespace ToDoList.Controllers
     public class ItemsController : Controller
     {
 
-      [HttpGet("/items")]
-      public ActionResult Index()
-      {
-          List<Item> allItems = Item.GetAll();
-          return View(allItems);
-      }
+        [HttpGet("/items")]
+        public ActionResult Index()
+        {
+            List<Item> allItems = Item.GetAll();
+            return View(allItems);
+        }
 
-      [HttpGet("/items/new")]
-      public ActionResult CreateForm()
-      {
-        return View();
-      }
+        [HttpGet("/items/new")]
+        public ActionResult CreateForm()
+        {
+            return View();
+        }
 
-      [HttpPost("/items")]
-      public ActionResult Create(string description)
-      {
-        Item myItem = new Item(description);
-        return RedirectToAction("Index");
-      }
+        [HttpPost("/items")]
+        public ActionResult Create(string description)
+        {
+            Item myItem = new Item(description);
+            return RedirectToAction("Index");
+        }
+        [HttpPost("/items/delete")]
+        public ActionResult DeleteAll()
+        {
+            Item.ClearAll();
+            return View();
+        }
     }
 }
